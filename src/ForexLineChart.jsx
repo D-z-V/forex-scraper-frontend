65cacb0e01c7b4d5af6e84411865417e211c2e1b
Dev Bhandari <dev.bhandari@hotmail.com>
2024-10-30 15:17:35 +0530

    added: loader

--- src/ForexLineChart.jsx
+++ src/ForexLineChart.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { LineChart } from '@mui/x-charts';
-import { Box, FormControl, InputLabel, Select, MenuItem, Button, Grid, useTheme, Typography } from '@mui/material';
+import { Box, FormControl, InputLabel, Select, MenuItem, Button, Grid, useTheme, Typography, Backdrop, CircularProgress } from '@mui/material';
 import { styled } from '@mui/material/styles';
 
 const StyledFormControl = styled(FormControl)(({ theme }) => ({
@@ -36,26 +36,31 @@ const ForexLineChart = () => {
     const [fromCurrency, setFromCurrency] = useState('USD');
     const [toCurrency, setToCurrency] = useState('INR');
     const [currencies, setCurrencies] = useState([]);
+    const [loading, setLoading] = useState(false);
   
     const API_URL = import.meta.env.VITE_FOREX_API_URL;
   
-    // Fetch supported currencies on component mount
-    useEffect(() => {
-      const fetchCurrencies = async () => {
-        try {
+    const fetchCurrencies = async () => {
+      setLoading(true);
+      setError(false);
+      try {
           const response = await fetch(`${API_URL}/api/supported-currencies`);
           if (response.ok) {
-            const data = await response.json();
-            setCurrencies(data); // Store currency list in state
+              const data = await response.json();
+              setCurrencies(data);
+              setLoading(false);
           } else {
-            console.error('Error fetching currencies');
+              throw new Error('Failed to fetch currencies');
           }
-        } catch (error) {
+      } catch (error) {
           console.error('Error fetching supported currencies:', error);
-        }
-      };
+          setTimeout(fetchCurrencies, 1000); 
+      }
+  };
+
+  useEffect(() => {
       fetchCurrencies();
-    }, []);
+  }, []);
   
     useEffect(() => {
       if (fromCurrency && toCurrency && fromCurrency !== toCurrency) {
@@ -130,200 +135,182 @@ const ForexLineChart = () => {
       <Typography variant="h4" align="center" sx={{ mb: 3, color: theme.palette.grey[300] }}>
         Exchange Rates History Tracker
       </Typography>
-      <Box sx={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        gap: 2,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <Box>
-          <StyledFormControl>
-            <InputLabel id="from_company">From Currency</InputLabel>
-            <Select
-              labelId="from_company"
-              label="From Currency"
-              value={fromCurrency}
-              onChange={(e) => setFromCurrency(e.target.value)}
-              MenuProps={{
-                PaperProps: {
-                  sx: {
-                    bgcolor: theme.palette.background.paper,
-                    color: theme.palette.grey[100],
-                  },
-                },
-              }}
-            >
-              {getAvailableFromCurrencies().map(([code, name]) => (
-                <MenuItem key={code} value={code}>{`${name} [${code}]`}</MenuItem>
-              ))}
-            </Select>
-          </StyledFormControl>
-        </Box>
-        <Box>
-          <StyledFormControl>
-            <InputLabel id="to_currency">To Currency</InputLabel>
-            <Select
-              labelId="to_currency"
-              label="To Currency"
-              value={toCurrency}
-              onChange={(e) => setToCurrency(e.target.value)}
-              MenuProps={{
-                PaperProps: {
-                  sx: {
-                    bgcolor: theme.palette.background.paper,
-                    color: theme.palette.grey[100],
-                  },
-                },
-              }}
-            >
-              {getAvailableToCurrencies().map(([code, name]) => (
-                <MenuItem key={code} value={code}>{`${name} [${code}]`}</MenuItem>
-              ))}
-            </Select>
-          </StyledFormControl>
-        </Box>
-        <Box>
-          <StyledFormControl>
-            <InputLabel id="period">Period</InputLabel>
-            <Select
+
+      <Backdrop open={loading} sx={{ color: '#fff', zIndex: theme.zIndex.drawer + 1 }}>
+        <CircularProgress color="inherit" />
+        {/* {retryCount > 0 && (
+          <Typography variant="body1" sx={{ ml: 2 }}>
+            Retrying... {retryCount} / {MAX_RETRIES}
+          </Typography>
+        )} */}
+      </Backdrop>
+
+      {!loading && (
+        <>
+          <Box sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: 2,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+            <StyledFormControl>
+              <InputLabel id="from_currency">From Currency</InputLabel>
+              <Select
+                labelId="from_currency"
+                label="From Currency"
+                value={fromCurrency}
+                onChange={(e) => setFromCurrency(e.target.value)}
+              >
+                {getAvailableFromCurrencies().map(([code, name]) => (
+                  <MenuItem key={code} value={code}>{`${name} [${code}]`}</MenuItem>
+                ))}
+              </Select>
+            </StyledFormControl>
+
+            <StyledFormControl>
+              <InputLabel id="to_currency">To Currency</InputLabel>
+              <Select
+                labelId="to_currency"
+                label="To Currency"
+                value={toCurrency}
+                onChange={(e) => setToCurrency(e.target.value)}
+              >
+                {getAvailableToCurrencies().map(([code, name]) => (
+                  <MenuItem key={code} value={code}>{`${name} [${code}]`}</MenuItem>
+                ))}
+              </Select>
+            </StyledFormControl>
+
+            <StyledFormControl>
+              <InputLabel id="period">Period</InputLabel>
+              <Select
                 labelId="period"
                 label="Period"
-              value={period}
-              onChange={(e) => setPeriod(e.target.value)}
-              MenuProps={{
-                PaperProps: {
-                  sx: {
-                    bgcolor: theme.palette.background.paper,
-                    color: theme.palette.grey[100],
-                  },
-                },
-              }}
-            >
-              <MenuItem value="1M">1 Month</MenuItem>
-              <MenuItem value="3M">3 Months</MenuItem>
-              <MenuItem value="6M">6 Months</MenuItem>
-            </Select>
-          </StyledFormControl>
-        </Box>
-        {/* <Box>
-          <StyledButton
-            variant="contained"
-            onClick={fetchForexData}
-          >
-            Fetch Data
-          </StyledButton>
-        </Box> */}
-      </Box>
-      <Box sx={{
-        height: 500,
-        bgcolor: theme.palette.background.default,
-        borderRadius: theme.shape.borderRadius,
-        p: 2,
-      }}>
+                value={period}
+                onChange={(e) => setPeriod(e.target.value)}
+              >
+                <MenuItem value="1M">1 Month</MenuItem>
+                <MenuItem value="3M">3 Months</MenuItem>
+                <MenuItem value="6M">6 Months</MenuItem>
+              </Select>
+            </StyledFormControl>
+          </Box>
 
-        <LineChart
-          xAxis={[{
-            label: 'Date',
-            labelStyle: chartTheme.axis.label,
-            dataKey: 'date',
-            scaleType: 'time',
-            tickLabelStyle: chartTheme.text,
-            stroke: chartTheme.axis.tick.stroke,
-          }]}
-          yAxis={[{
-            labelStyle: chartTheme.axis.label,
-            tickLabelStyle: chartTheme.text,
-            stroke: chartTheme.axis.tick.stroke,
-          }]}
-          series={[{
-            dataKey: 'high_rate',
-            label: 'High Rate',
-            color: theme.palette.primary.main,
-            showMark: true,
-            area: { fill: theme.palette.primary.light, opacity: 0.2 },
-            baseline: 'min'
-          },
-          {
-            dataKey: 'low_rate',
-            label: 'Low Rate',
-            color: theme.palette.secondary.main,
-            showMark: true,
-            area: { fill: theme.palette.secondary.light, opacity: 0.2 },
-            baseline: 'min'
-          }]}
+
+
+
+        <Box sx={{
+          height: 500,
+          bgcolor: theme.palette.background.default,
+          borderRadius: theme.shape.borderRadius,
+          p: 2,
+        }}>
+
+          <LineChart
+            xAxis={[{
+              label: 'Date',
+              labelStyle: chartTheme.axis.label,
+              dataKey: 'date',
+              scaleType: 'time',
+              tickLabelStyle: chartTheme.text,
+              stroke: chartTheme.axis.tick.stroke,
+            }]}
+            yAxis={[{
+              labelStyle: chartTheme.axis.label,
+              tickLabelStyle: chartTheme.text,
+              stroke: chartTheme.axis.tick.stroke,
+            }]}
+            series={[{
+              dataKey: 'high_rate',
+              label: 'High Rate',
+              color: theme.palette.primary.main,
+              showMark: true,
+              area: { fill: theme.palette.primary.light, opacity: 0.2 },
+              baseline: 'min'
+            },
+            {
+              dataKey: 'low_rate',
+              label: 'Low Rate',
+              color: theme.palette.secondary.main,
+              showMark: true,
+              area: { fill: theme.palette.secondary.light, opacity: 0.2 },
+              baseline: 'min'
+            }]}
+            
+            title="High and Low Exchange Rates"
+            legend={{ position: 'top' }}  // Positioning the legend at the top
+            sx={{
+              '.MuiLineElement-root': {
+                strokeWidth: 2,
+              },
+              '.MuiMarkElement-root': {
+                strokeWidth: 2,
+                r: 4,
+              },
+              my: { sm: 0, md: 2 },
+            }}
+            height={500}
+            margin={{ left: 70, right: 30, top: 30, bottom: 70 }}
+            grid={{ vertical: true, horizontal: true, stroke: chartTheme.grid.stroke }}
+            dataset={data}
+            bgcolor={chartTheme.background.color}
+          />
+
+          <br />
+
+          <LineChart
+            xAxis={[{
+              label: 'Date',
+              labelStyle: chartTheme.axis.label,
+              dataKey: 'date',
+              scaleType: 'time',
+              tickLabelStyle: chartTheme.text,
+              stroke: chartTheme.axis.tick.stroke,
+            }]}
+            yAxis={[{
+              labelStyle: chartTheme.axis.label,
+              tickLabelStyle: chartTheme.text,
+              stroke: chartTheme.axis.tick.stroke,
+            }]}
+            series={[{
+              dataKey: 'open_rate',
+              label: 'Open Rate',
+              color: theme.palette.primary.main,
+              showMark: true,
+            },
+            {
+              dataKey: 'close_rate',
+              label: 'Close Rate',
+              color: theme.palette.secondary.main,
+              showMark: true,
+            }]}
+            title="Open and Close Exchange Rates"
+            legend={{ position: 'top' }}  
+            sx={{
+              '.MuiLineElement-root': {
+                strokeWidth: 2,
+              },
+              '.MuiMarkElement-root': {
+                strokeWidth: 2,
+                r: 4,
+              },
+              my: { sm: 0, md: 2 },
+            }}
+            height={500}
+            margin={{ left: 70, right: 30, top: 30, bottom: 70 }}
+            grid={{ vertical: true, horizontal: true, stroke: chartTheme.grid.stroke }}
+            dataset={data}
+            bgcolor={chartTheme.background.color}
+          />
+
+          <br />
           
-          title="High and Low Exchange Rates"
-          legend={{ position: 'top' }}  // Positioning the legend at the top
-          sx={{
-            '.MuiLineElement-root': {
-              strokeWidth: 2,
-            },
-            '.MuiMarkElement-root': {
-              strokeWidth: 2,
-              r: 4,
-            },
-            my: { sm: 0, md: 2 },
-          }}
-          height={500}
-          margin={{ left: 70, right: 30, top: 30, bottom: 70 }}
-          grid={{ vertical: true, horizontal: true, stroke: chartTheme.grid.stroke }}
-          dataset={data}
-          bgcolor={chartTheme.background.color}
-        />
-
-        <br />
-
-        <LineChart
-          xAxis={[{
-            label: 'Date',
-            labelStyle: chartTheme.axis.label,
-            dataKey: 'date',
-            scaleType: 'time',
-            tickLabelStyle: chartTheme.text,
-            stroke: chartTheme.axis.tick.stroke,
-          }]}
-          yAxis={[{
-            labelStyle: chartTheme.axis.label,
-            tickLabelStyle: chartTheme.text,
-            stroke: chartTheme.axis.tick.stroke,
-          }]}
-          series={[{
-            dataKey: 'open_rate',
-            label: 'Open Rate',
-            color: theme.palette.primary.main,
-            showMark: true,
-          },
-          {
-            dataKey: 'close_rate',
-            label: 'Close Rate',
-            color: theme.palette.secondary.main,
-            showMark: true,
-          }]}
-          title="Open and Close Exchange Rates"
-          legend={{ position: 'top' }}  
-          sx={{
-            '.MuiLineElement-root': {
-              strokeWidth: 2,
-            },
-            '.MuiMarkElement-root': {
-              strokeWidth: 2,
-              r: 4,
-            },
-            my: { sm: 0, md: 2 },
-          }}
-          height={500}
-          margin={{ left: 70, right: 30, top: 30, bottom: 70 }}
-          grid={{ vertical: true, horizontal: true, stroke: chartTheme.grid.stroke }}
-          dataset={data}
-          bgcolor={chartTheme.background.color}
-        />
-
-        <br />
-        
-      </Box>
-
+        </Box>
+        </>
+      )}
     </Box>
+    
   );
 };
 
